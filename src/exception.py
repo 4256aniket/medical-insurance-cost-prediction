c8c11196f04d597ac80eb7a6a4cd1c0925b0ae2f
Aniket Kumar <aniketkumar@Anikets-Air.lan>
2024-11-10 17:30:19 +0530

    solve src module not found and remove target col from columnTransformer in data_transformation

--- src/exception.py
+++ src/exception.py
@@ -22,6 +22,6 @@ if __name__=='__main__':
     try:
         a=1/0
     except Exception as e:
-        logging.info('Divison by zero')
+        logging.info("Divison by zero")
         raise CustomException(e, sys)
      
